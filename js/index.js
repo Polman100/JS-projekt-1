76d1134712bed319f11edec71ef81845faba2e67
Marcin <mpolman100@gmail.com>
2022-06-08 13:53:13 +0200

    adding new and modyfing functions

--- js/index.js
+++ js/index.js
@@ -20,8 +20,8 @@ const outcomeList = getElement("outcome-list");
 
 // declarations
 
-const incomeArray = []
-const outcomeArray = []
+let incomeArray = []
+let outcomeArray = []
 
 // let nanoId = nanoid()
 
@@ -36,22 +36,124 @@ const addNewObjectToArray = (text, value, array) => {
     array.push(object);
 };
 
-const createElement = (item) => {
-    const incomeItem = create("li");
-    incomeItem.className = "item-class";
-    incomeItem.id = item.id;
-    incomeItem.innerText = item.name + " - " + item.amount + "zł";
-    incomeList.appendChild(incomeItem);
+const removeObjectFromArray = (id, array) => {
+    if (array = incomeArray) {
+        const newArray = array.filter((item) => item.id !== id);
+        incomeArray = newArray;
+    }
+    if (array = outcomeArray) {
+        const newArray = array.filter((item) => item.id !== id);
+        outcomeArray = newArray;
+    }   
+}
+
+const editObjectInArray = (id, array, newText, newValue) => {
+    if (array = incomeArray) {
+        const newArray = array.filter((item) => item.id !== id);
+        incomeArray = newArray;
+    }
+    if (array = outcomeArray) {
+        array.find((item) => item.id == id).name = newText;
+        array.find((item) => item.id == id).amount = newValue;
+        console.log(outcomeArray);
+        // objectBeingEdited.name = newText;
+        // objectBeingEdited.value = newValue;
+        createLi(outcomeList, outcomeArray);
+    }   
+    
+    // if (array = outcomeArray) {
+    //     array.find((item) => item.id == id);
+    //     let properties = {
+    //         "name": newText,
+    //         "value": newValue,
+    //     };
+    //     createLi(outcomeList, outcomeArray)
+    // }   
+} 
+
+
+
+const createIncomeElement = (item, list, array) => {
+    const listItem = create("li");
+    listItem.className = "item-class";
+    listItem.id = item.id;
+    listItem.innerText = item.name + " - " + item.amount + "zł";
+    list.appendChild(listItem);
+
+    createEditButton(item, listItem, array, item.name, item.amount);
+    createDeleteButton(item, listItem, array);
 };
 
 
-const createLi = () => {
-    incomeList.innerHTML = ''
-    incomeArray.forEach(item =>{
-        createElement(item)
+const createLi = (list, array) => {
+    list.innerHTML = ''
+    array.forEach(item => {
+        createIncomeElement(item, list, array);
     })
 };
 
+const createEditButton = (item, parent, array, previousName, previousValue) => {
+    const editButton = create("button");
+    editButton.innerText = "Edytuj";
+    editButton.id = item.id;
+    editButton.className = "positions-button";
+    parent.appendChild(editButton);
+    editButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        // console.log(item.id);
+        const itemToEdit = getElement(item.id);
+        console.log(itemToEdit);
+        itemToEdit.innerText = '';
+        const editForm = create("form");
+        editForm.className = "edit-form"
+        const editFormTextField = create("input");
+        editFormTextField.id = "edit-name";
+        editFormTextField.type = "text"
+        editFormTextField.className = "edit-text-field"
+        editFormTextField.value = previousName
+        const editFormValueField = create("input");
+        editFormValueField.id = "edit-value";
+        editFormValueField.type = "number"
+        editFormValueField.className = "edit-value-field"
+        // console.log(previousValue)
+        editFormValueField.value = previousValue
+        editForm.append(editFormTextField, editFormValueField);
+        // saveButton
+        const saveButton = create("button")
+        saveButton.innerText = "Zapisz"
+        saveButton.id = item.id
+        saveButton.className = "save-button";
+        saveButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            const newText = getElement("edit-name")
+            const newValue = getElement("edit-value")
+            editObjectInArray(item.id, array, newText.value, newValue.value);
+
+        });
+
+        itemToEdit.append(editForm, saveButton)
+        
+        
+        // itemToEdit.removeChild(button.className = "positions-button");
+
+        
+
+    })
+};
+
+const createDeleteButton = (item, parent, array) => {
+    const deleteButton = create("button");
+    deleteButton.innerText = "Usuń";
+    deleteButton.id = item.id;
+    deleteButton.className = "delete-button";
+    parent.appendChild(deleteButton)
+    deleteButton.addEventListener("click", (e) => {
+        const itemToRemove = getElement(parent.id);
+        itemToRemove.remove();                          // <<-- MIND BLOWN, że to działa (nie do końca)
+        removeObjectFromArray(item.id, array);
+        console.log(item.id, array)
+    });
+}   
 
 
 // events
@@ -72,77 +174,51 @@ const addIncome = incomeButton.addEventListener("click", (e) => {
     addNewObjectToArray(incomeName.value, incomeValue.value, incomeArray);
     console.log(incomeArray);
 
-    createLi();
+    createLi(incomeList, incomeArray);
 
-
-
-    // const incomeItem = create("li");
-    // incomeItem.className = "item-class";
-    // incomeItem.id = nanoId;
-    // incomeItem.innerText = incomeName.value + " - ";
-
-    // const incomeItemValue = create("span");        //   <<----- czy to najlepsze podejście?
-    // incomeItemValue.innerText = incomeValue.value + "zł";
-
-    const editButton = create("button");
-    editButton.innerText = "Edytuj";
-    editButton.id = "edit-income";
-    editButton.className = "positions-button";
-    
-
-    const deleteButton = create("button");
-    deleteButton.innerText = "Usuń";
-    deleteButton.id = "delete-income";
-    deleteButton.className = "delete-button";
-    deleteButton.addEventListener("click", (e) => {
-        const itemToRemove = getElement(incomeItem.id);
-        itemToRemove.remove();                          // <<-- MIND BLOWN, że to działa (nie do końca)
-    });                                                 // no i znowu tego nie można rozbić bo czerpie z incomeItem
-    // deleteButton.onclick = incomeItem.remove(incomeList);
-
-    
-    // appendToElement(incomeItem, incomeItemValue);
-    // appendToElement(incomeItem, editButton);
-    // appendToElement(incomeItem, deleteButton);
-    // appendToElement(incomeList, incomeItem);
-
-    // + incomeValue.Value + "zł";  <<---- ciekawi mnie czemu to wyrzucało undefined przy dodaniu do incomeItem.innerText
 });
 
 const addOutcome = outcomeButton.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("outcomeButton is clicked");
-    console.log(outcomeName.value);
-    console.log(outcomeValue.value);
+    // console.log("outcomeButton is clicked");
+    // console.log(outcomeName.value);
+    // console.log(outcomeValue.value);
 
-    const outcomeItem = create("li");
-    outcomeItem.className = "item-class";
-    outcomeItem.id = "item-id";
-    outcomeItem.innerText = outcomeName.value + " - ";
+    addNewObjectToArray(outcomeName.value, outcomeValue.value, outcomeArray);
+    console.log(outcomeArray);
 
-    const outcomeItemValue = create("span");
-    outcomeItemValue.innerText = outcomeValue.value + "zł";
+    createLi(outcomeList, outcomeArray);
 
-    const editButton = create("button");
-    editButton.innerText = "Edytuj";
-    editButton.id = "edit-outcome";
-    editButton.className = "positions-button";
 
-    const deleteButton = create("button");
-    deleteButton.innerText = "Usuń";
-    deleteButton.id = "delete-outcome";
-    deleteButton.className = "delete-button";
-    deleteButton.addEventListener("click", (e) => {
-        const itemToRemove = getElement(incomeItem.id);
-        itemToRemove.remove();                          // <<-- MIND BLOWN, że to działa
-    });
+
+    // const outcomeItem = create("li");
+    // outcomeItem.className = "item-class";
+    // outcomeItem.id = "item-id";
+    // outcomeItem.innerText = outcomeName.value + " - ";
+
+    // const outcomeItemValue = create("span");
+    // outcomeItemValue.innerText = outcomeValue.value + "zł";
+
+    // const editButton = create("button");
+    // editButton.innerText = "Edytuj";
+    // editButton.id = "edit-outcome";
+    // editButton.className = "positions-button";
+
+    // const deleteButton = create("button");
+    // deleteButton.innerText = "Usuń";
+    // deleteButton.id = "delete-outcome";
+    // deleteButton.className = "delete-button";
+    // deleteButton.addEventListener("click", (e) => {
+    //     const itemToRemove = getElement(incomeItem.id);
+    //     itemToRemove.remove();                          // <<-- MIND BLOWN, że to działa
+    // });
     
 
     
-    appendToElement(outcomeItem, outcomeItemValue);
-    appendToElement(outcomeItem, editButton);
-    appendToElement(outcomeItem, deleteButton);
-    appendToElement(outcomeList, outcomeItem);
+    // appendToElement(outcomeItem, outcomeItemValue);
+    // appendToElement(outcomeItem, editButton);
+    // appendToElement(outcomeItem, deleteButton);
+    // appendToElement(outcomeList, outcomeItem);
 
 });
 
